44059ea1dd422f030e35d0d4d2e3ef27c8929d6c
Kayo Phoenix <kayo@illumium.org>
2018-09-30 22:28:18 +0500

    Using .init_array section instead of .ctors

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-#[cfg_attr(target_os = "linux", link_section = ".ctors")]
-pub static INITIALIZE: extern "C" fn() = ::myplugin_initialize;
+//#[cfg_attr(target_os = "linux", link_section = ".ctors")]
+#[cfg_attr(target_os = "linux", link_section = ".init_array")]
+pub static INITIALIZE: extern "C" fn() = myplugin_initialize;
 
 #[no_mangle]
 pub extern "C" fn myplugin_initialize() {
